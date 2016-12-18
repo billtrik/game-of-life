ea6c4d01c1fec73ef21c9c5bc82a43875d3e6c6a
Trikalinos, Vasileios [R3Pi] <Vasileios.Trikalinos@r3pi.io>
2016-12-18 23:40:37 +0100

    Create dummy table data on App init

--- src/initial-state.js
+++ src/initial-state.js
@@ -1,4 +1,30 @@
 import _ from 'lodash';
 import { fromJS } from 'immutable';
+import {WIDTH, HEIGHT} from './constants';
 
-export default () => fromJS({});
+// from: http://stackoverflow.com/a/36756561/659610
+function getRandomBoolean () {
+  const a = new Uint8Array(1);
+  crypto.getRandomValues(a);
+  return a[0] < 127;
+}
+
+export function generateDummyData(width, height) {
+  const result = [];
+
+  for (let i=0; i < width; i++) {
+    result[i] = [];
+
+    for (let j=0; j < width; j++) {
+      result[i][j] = getRandomBoolean();
+    }
+  }
+
+  return result;
+}
+
+export default () => fromJS({
+  width: WIDTH,
+  height: HEIGHT,
+  data: generateDummyData(WIDTH, HEIGHT)
+});
