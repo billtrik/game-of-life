d703d2e2bfab84e837f60767b0c42a40f2f62969
Trikalinos, Vasileios [R3Pi] <Vasileios.Trikalinos@r3pi.io>
2016-12-19 01:03:34 +0100

    Implement logic that calculates next state of data

--- src/js/reducer.js
+++ src/js/reducer.js
@@ -1,14 +1,50 @@
-import Immutable from 'immutable';
-import {NEXT_STEP} from './actions';
+import _ from 'lodash';
+import { fromJS } from 'immutable';
+import { NEXT_STEP } from './actions';
 
-function calculateNextState(data) {
-  return data;
+function getRowCount(list, row, col, width) {
+  const slice = list.slice(Math.max(0, col - 1), Math.min(width, col + 2));
+
+  return _.compact(slice.toJS()).length
 }
 
-export default (state = Immutable.fromJS({}), action) => {
+function shouldLive(data, row, col, width, height) {
+  let total = 0;
+
+  // Get count for previous line
+  if (row !== 0) {
+    total = total + getRowCount(data.get(row - 1), row, col, width);
+  }
+
+  // Get count for current line
+  total = total + getRowCount(data.get(row), row, col, width);
+
+  // Get count for next line
+  if (row !== height - 1) {
+    total = total + getRowCount(data.get(row + 1), row, col, width);
+  }
+
+  return total === 2 || total === 3;
+}
+
+function calculateNextState(data, width, height) {
+  const result = [];
+
+  for (let i=0; i < width; i++) {
+    result[i] = [];
+
+    for (let j=0; j < width; j++) {
+      result[i][j] = shouldLive(data, i, j, width, height);
+    }
+  }
+
+  return fromJS(result);
+}
+
+export default (state = fromJS({}), action) => {
   switch(action.type) {
     case NEXT_STEP:
-      return state.set('data', calculateNextState(state.get('data')));
+      return state.set('data', calculateNextState(state.get('data'), state.get('width'), state.get('height')));
     default:
       return state;
   }
